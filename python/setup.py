06b3639cb83e8c724d92513798fb069dbe707207
James Bowman <jamesb@excamera.com>
2019-02-11 19:18:56 -0800

    Start of Python import

---
+++ python/setup.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+from setuptools import setup
+
+LONG = """\
+I2CDriver is a tool for controlling, monitoring and capturing I2C from your PC's USB port. It connects as a standard USB serial device, so there are no drivers to install."""
+
+for l in open("i2cdriver.py", "rt"):
+    if l.startswith("__version__"):
+        exec(l)
+
+setup(name='i2cdriver',
+      version=__version__,
+      author='James Bowman',
+      author_email='jamesb@excamera.com',
+      url='http://i2cdriver.com',
+      description='I2CDriver is a desktop I2C interface',
+      long_description=LONG,
+      license='GPL',
+      py_modules=['i2cdriver'],
+      install_requires=['pyserial']
+      )
